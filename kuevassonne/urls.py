2c99755631a6550d9f237044f3ae9054a6a96a2d
PatillaCode <patillacode@gmail.com>
2021-08-20 04:04:12 +0200

    static files url

--- kuevassonne/urls.py
+++ kuevassonne/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('website.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path('admin/', admin.site.urls),
+        path('accounts/', include('django.contrib.auth.urls')),
+        path('', include('website.urls')),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + staticfiles_urlpatterns()
+)
