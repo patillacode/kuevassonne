322c2c74274eea4b8d7e3be6ce7321dae8847544
Piers Olenski <hello@piers.io>
2021-10-03 06:50:13 +0100

    Remove dupe code

--- website/static/css/tailwind.config.js
+++ website/static/css/tailwind.config.js
@@ -5,21 +5,6 @@ module.exports = {
     fontFamily: { text: ['Spiegel'], display: ['Beaufort'] },
     extend: {
       colors: {
-        // black: '#111111',
-        // white: '#f9f9f9',
-        // gold: '#a88a50',
-        // silver: '#999999',
-        // bronce: '#cd7f32',
-        // green: '#008a19',
-        // pink: '#a850a4',
-        // blue: '#008bf3',
-        // cyan: '#22D3EE',
-        // yellow: '#fffe00',
-        // brown: '#6f4615',
-        // orange: '#ff6900',
-        // red: '#8a0000',
-        // winter: '#e6dada',
-        // dark: '#274046'
         black: '#111111',
         white: '#f9f9f9',
         gold: '#a88a50',
